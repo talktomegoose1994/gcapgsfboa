fdbd40a2fedac10e0d7df00d6e692610cc9e6234
nathanaelrobinson <naterobinson@AVA.local>
2018-10-25 22:15:49 -0500

    addtional small changes to scripts file :wq
    i#
    ˆ:wq

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var app=angular.module('single-page-app',['ngRoute']);
+var app=angular.module('single-page-app',['ngRoute', './lib/index.js']);
 
 
 app.config(function($routeProvider){
@@ -20,13 +20,5 @@ app.config(function($routeProvider){
 app.controller('cfgController',function($scope){
 
       $scope.message="";
-      //
-      // var sendTweet(message){
-      //   var text = "Overheard at GC: " + message;
-      //   return ""
-      // };
-
-      // API KEY = MS7Y5S6WTM0KDdghTTM9JEqco
-      // SECRET KEY = Eunc2AeV4SXnozR9Q9xhVOBDVEQ2DOWztGpnzRea9DMJw0AFNd
 
 });
